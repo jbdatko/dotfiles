5dc1cd3e6b731ca94ccb972e9ecec8c88292bc64
Josh Datko <jbdatko@gmail.com>
2013-06-07 21:17:01 -0400

    Cleanup

--- emacs/init/themes.el
+++ emacs/init/themes.el
@@ -1,10 +1,9 @@
+;;Emacs 24 uses a different theme package
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'zenburn t)
+
+;; This is the Emacs 23 way
 ;(require 'color-theme)
 ;(color-theme-initialize)
 ;(load-file "~/.emacs.d/color-theme-wombat.el")
 ;(color-theme-wombat)
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/")
-;;(load-theme 'wombat t)
-;;(load-theme 'cyberpunk t)
-(load-theme 'zenburn t)
